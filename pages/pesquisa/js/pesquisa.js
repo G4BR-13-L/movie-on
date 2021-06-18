fda1e0bf879e9720060ad8181be9eefcd9d23497
G4BR-13-L <gabriel.victorc13@gmail.com>
2021-06-18 20:44:09 -0300

    implementada a função de pesquisa

--- pages/pesquisa/js/pesquisa.js
+++ pages/pesquisa/js/pesquisa.js
@@ -1,35 +1,48 @@
 
-function inicializarLancamentos() {
+
+
+
+function pesquisar() {
+    let queryDePesquisa = document.querySelector('input.pesquisa').value
     let xhr1 = new XMLHttpRequest();
     xhr1.onload = carregarFilmes;
-    xhr1.open('GET', 'https://api.themoviedb.org/3/tv/popular?api_key=44d302b5c67a19b15c6effc9654af134&language=pt-BR&page=1')
+    xhr1.open('GET', `https://api.themoviedb.org/3/search/movie?api_key=44d302b5c67a19b15c6effc9654af134&language=en-US&query=${queryDePesquisa}&page=1&include_adult=false`)
     xhr1.send();
 }
 
 function carregarFilmes() {
-    //console.log(this.responseText);
     var dados = JSON.parse(this.responseText);
     console.log(dados);
-    for (let i = 0; i < dados.results.length; i++) {
+    var container = document.querySelector('div.resultado-pesquisa')
+    if (dados.results.length == 0) {
+        container.innerHTML = `
+            <div class="nada-encontrado">
+                <i class='bx bxs-meteor'></i>
+                <h2>Nada encontrado</h2>
+            </div>
+        `
+    } else {
+        container.innerHTML = ``;
+        for (let i = 0; i < dados.results.length; i++) {
             renderizarFilmes(dados.results[i],);
-        
-        /*28 - "Ação"
-        12 - "Aventura"	
-        16 - "Animação"
-        35 - "Comédia"
-        18 - Drama*/
+            /*28 - "Ação"
+            12 - "Aventura"	
+            16 - "Animação"
+            35 - "Comédia"
+            18 - Drama*/
+        }
     }
 }
 function renderizarFilmes(dadosFilme) {
-    var container = document.querySelector(`div.container-series`)
-    var newDate = dadosFilme.first_air_date.split('-');
+    var container = document.querySelector(`div.resultado-pesquisa`)
+    var newDate = dadosFilme.release_date.split('-');
     var preRender = [];
     let preRenderText = `
-            <div class="movie-card-series">
-                <div class="movie-poster-series">
+            <div class="movie-card-pesquisa">
+                <div class="movie-poster-pesquisa">
                     <img src="https://image.tmdb.org/t/p/w185${dadosFilme.poster_path}" alt="">
                 </div>
-                <div class="movie-info-series">
+                <div class="movie-info-pesquisa">
                     <ul>
                         <li>${dadosFilme.original_title}</li>
                         <li>${newDate[0]}</li>
@@ -40,7 +53,7 @@ function renderizarFilmes(dadosFilme) {
     preRender.push(preRenderText);
     preRender.join("");
     container.innerHTML += preRender;
-    
+
 }
 /*
 
