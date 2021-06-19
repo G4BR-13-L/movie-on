276433566bf0ae70b889f8d5a7e49105b9be1e30
G4BR-13-L <gabriel.victorc13@gmail.com>
2021-06-18 22:51:30 -0300

    teste de rota

--- js/mostrarDetalhes.js
+++ js/mostrarDetalhes.js
@@ -1,5 +1,5 @@
 function mostrarDetalhes(id){
     localStorage.setItem('filme_id', id);
     console.log('Detalhes do filme '+ id)
-    window.location.replace("/pages/detalhes/detalhes.html")
+    window.location.replace("/movie-on/pages/detalhes/detalhes.html")
 }
